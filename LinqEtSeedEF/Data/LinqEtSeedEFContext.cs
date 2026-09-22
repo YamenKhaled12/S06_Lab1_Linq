4db067cfc786fe75ec42c8fc266820afda866b26
2517264 <2517264@LLBINFD060222>
2026-09-22 09:19:01 -0400

    update data et verfication

--- LinqEtSeedEF/Data/LinqEtSeedEFContext.cs
+++ LinqEtSeedEF/Data/LinqEtSeedEFContext.cs
@@ -5,7 +5,7 @@ namespace LinqEtSeedEF.Data
 {
     public class LinqEtSeedEFContext : DbContext
     {
-        public LinqEtSeedEFContext (DbContextOptions<LinqEtSeedEFContext> options)
+        public LinqEtSeedEFContext(DbContextOptions<LinqEtSeedEFContext> options)
             : base(options)
         {
         }
@@ -20,7 +20,7 @@ namespace LinqEtSeedEF.Data
                 new Client { Id = 3, Nom = "Sophie Dubois", Adresse = "789 avenue des Érables, Trois-Rivières", Telephone = "(819) 555-9012" },
                 new Client { Id = 4, Nom = "Jean-François Lavoie", Adresse = "1010 rue Saint-Denis, Montréal", Telephone = "(514) 555-3456" },
                 new Client { Id = 5, Nom = "Geneviève Parent", Adresse = "111 rue des Pionniers, Québec", Telephone = "(418) 555-7890" }
-                );
+            );
 
             builder.Entity<Restaurant>().HasData(
                 new Restaurant { Id = 1, Nom = "La graine du père George", Adresse = "456 rue Sainte-Catherine, Montréal", Telephone = "(514) 555-2345" },
@@ -28,7 +28,7 @@ namespace LinqEtSeedEF.Data
                 new Restaurant { Id = 3, Nom = "La Belle Province", Adresse = "1010 boulevard René-Lévesque, Trois-Rivières", Telephone = "(819) 555-2345" },
                 new Restaurant { Id = 4, Nom = "La Piazzetta", Adresse = "1111 rue de la Montagne, Montréal", Telephone = "(514) 555-6789" },
                 new Restaurant { Id = 5, Nom = "Le Petit Coin", Adresse = "2222 rue Saint-Jean, Québec", Telephone = "(418) 555-2345" }
-                );
+            );
 
             builder.Entity<Plat>().HasData(
                 new Plat { Id = 1, Nom = "Sauté de légumes", Description = "Un mélange de légumes frais sautés à la poêle", Prix = 8.99m, RestaurantId = 1, Vegetarien = true },
@@ -37,39 +37,51 @@ namespace LinqEtSeedEF.Data
                 new Plat { Id = 4, Nom = "Tofu au gingembre", Description = "Un délicieux plat de tofu avec du gingembre frais", Prix = 11.99m, RestaurantId = 2, Vegetarien = true },
                 new Plat { Id = 5, Nom = "Poutine", Description = "Patate, sauce, fromage skwich skwich", Prix = 7.99m, RestaurantId = 3, Vegetarien = false },
                 new Plat { Id = 6, Nom = "Pizza", Description = "Pepperonni fromage", Prix = 9.99m, RestaurantId = 4, Vegetarien = false }
-                );
+            );
 
             builder.Entity<Commande>().HasData(
                 new Commande { Id = 1, Date = new DateTime(2022, 05, 01), ClientId = 1, RestaurantId = 1 },
                 new Commande { Id = 2, Date = new DateTime(2022, 05, 02), ClientId = 2, RestaurantId = 2 },
                 new Commande { Id = 3, Date = new DateTime(2022, 05, 03), ClientId = 3, RestaurantId = 3 },
-                new Commande { Id = 4, Date = new DateTime(2022, 05, 03), ClientId = 2, RestaurantId = 4 }
-                );
+                new Commande { Id = 4, Date = new DateTime(2022, 05, 03), ClientId = 2, RestaurantId = 4 },
+
+                // Nouvelle commande du 4 mai 2022 au Bistro
+                new Commande { Id = 5, Date = new DateTime(2022, 05, 04), ClientId = 2, RestaurantId = 2 }
+            );
 
             builder.Entity<CommandePlat>().HasData(
                 new CommandePlat { CommandeId = 1, PlatId = 1, Quantite = 1 },
                 new CommandePlat { CommandeId = 1, PlatId = 2, Quantite = 2 },
-                
+
                 new CommandePlat { CommandeId = 2, PlatId = 3, Quantite = 2 },
                 new CommandePlat { CommandeId = 2, PlatId = 4, Quantite = 1 },
-                
+
                 new CommandePlat { CommandeId = 3, PlatId = 5, Quantite = 4 },
-                
-                new CommandePlat { CommandeId = 4, PlatId = 6, Quantite = 2 }
-                );
+
+                new CommandePlat { CommandeId = 4, PlatId = 6, Quantite = 2 },
+
+                // Plats de la nouvelle commande
+                // 3 x Poulet au gingembre
+                new CommandePlat { CommandeId = 5, PlatId = 3, Quantite = 3 },
+
+                // 3 x Tofu au gingembre
+                new CommandePlat { CommandeId = 5, PlatId = 4, Quantite = 3 }
+            );
 
             builder.Entity<TestData>().HasData(
                 new TestData { Id = 1, ValeurA = 1, ValeurB = 379 },
                 new TestData { Id = 2, ValeurA = 85, ValeurB = 65000 }
-                );
+            );
 
-            // Lorsque l'on efface une entrée CommandePlate, on ne veut pas effacer la commande
+            // Lorsque l'on efface une entrée CommandePlate,
+            // on ne veut pas effacer la commande
             builder.Entity<CommandePlat>()
-            .HasOne(cp => cp.Commande)
-            .WithMany(c => c.CommandesPlats)
-            .OnDelete(DeleteBehavior.NoAction);
+                .HasOne(cp => cp.Commande)
+                .WithMany(c => c.CommandesPlats)
+                .OnDelete(DeleteBehavior.NoAction);
 
-            // Lorsque l'on efface une entrée CommandePlate, on ne veut pas effacer le plat
+            // Lorsque l'on efface une entrée CommandePlate,
+            // on ne veut pas effacer le plat
             builder.Entity<CommandePlat>()
                 .HasOne(cp => cp.Plat)
                 .WithMany(p => p.CommandesPlats)
